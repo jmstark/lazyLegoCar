fcbb5161343e19feac21e97eb2795ddce16411b1
kjkuhn <kuhn@in.tum.de>
2013-12-03 14:21:52 +0100

    added socket for raspberry

---
+++ raspberry/socketTest.cpp
@@ -0,0 +1,30 @@
+#include "socket.h"
+
+using namespace std;
+using namespace rasp_sock;
+
+int main(void){
+	cout << "starting socket" << endl;
+	RaspberrySocket s;
+	s = RaspberrySocket();
+	if(!s.initSocket()){
+		cout << "failed open socket" << endl;
+		return 0;
+	}
+	cout << "initialized socket, waiting for client to connect..." << endl;
+	if(!s.waitForClient()){
+		cout << "failed to accept client" << endl;
+		return 0;
+	}
+	cout << "client connected" << endl;
+	string rd;
+	while(1){
+		rd = s.receive();
+		if(rd.compare("") == 0){
+			cout << "the client disconnected from the server, shutting down" << endl;
+			break;
+		}
+		cout << rd << endl;
+	}
+	return 0;
+}
