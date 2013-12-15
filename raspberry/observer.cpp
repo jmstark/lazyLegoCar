9238c02db2e8195e396508796dad0429934b4238
kjkuhn <kuhn@in.tum.de>
2013-12-15 12:20:35 +0100

    fix

--- raspberry/observer.cpp
+++ raspberry/observer.cpp
@@ -17,11 +17,13 @@ namespace obs{
 		while(1){
 			if(toArduino.changed.load(std::memory_order_relaxed)){
 				toArduino.mtx.lock();
+				toArduino.changed.store(false, std::memory_order_relaxed);
 				pipe->usbWrite((void*)&toArduino.com, sizeof com);
 				toArduino.mtx.unlock();
 			}
 			if(fromArduino.changed.load(std::memory_order_relaxed)){
 				fromArduino.mtx.lock();
+				fromArduino.changed.store(false, std::memory_order_relaxed);
 				str = createJSON(&fromArduino.com);
 				fromArduino.mtx.unlock();
 				sock->sendMsg(str);
