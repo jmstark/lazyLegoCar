4a525c8decab57bf1b0f6b70f860ea3f7b7ffb76
tiepdo <viettiep.do@live.de>
2014-01-14 23:09:34 +0100

    app verbessert

--- HelloWorld/gen/com/example/helloworld/R.java
+++ HelloWorld/gen/com/example/helloworld/R.java
@@ -13,6 +13,9 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int custom_barcolor=0x7f080000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -29,26 +32,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int progressshape=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
-        public static final int button_backward=0x7f090005;
-        public static final int button_forward=0x7f090004;
-        public static final int button_left=0x7f090007;
-        public static final int button_main=0x7f090008;
-        public static final int button_right=0x7f090006;
-        public static final int reading=0x7f090002;
-        public static final int seekbar=0x7f090001;
-        public static final int textView1=0x7f090003;
-        public static final int textView2=0x7f090000;
+        public static final int action_settings=0x7f0a000b;
+        public static final int button_backward=0x7f0a0005;
+        public static final int button_forward=0x7f0a0004;
+        public static final int button_left=0x7f0a0008;
+        public static final int button_main=0x7f0a0009;
+        public static final int button_right=0x7f0a0006;
+        public static final int button_straight=0x7f0a0007;
+        public static final int progressshape=0x7f0a000a;
+        public static final int reading=0x7f0a0002;
+        public static final int seekbar=0x7f0a0001;
+        public static final int textView1=0x7f0a0003;
+        public static final int textView2=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_left_click=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int left_click=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int left_click=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
         public static final int Backwards=0x7f060008;
@@ -58,11 +64,12 @@ public final class R {
         public static final int Left=0x7f060006;
         public static final int OK=0x7f060003;
         public static final int Right=0x7f060007;
-        public static final int SpeedSetting=0x7f06000a;
+        public static final int SpeedSetting=0x7f06000b;
+        public static final int Straight=0x7f06000a;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
-        public static final int title_activity_left_click=0x7f06000b;
+        public static final int title_activity_left_click=0x7f06000c;
     }
     public static final class style {
         /** 
--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.helloworld;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -9,9 +11,11 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,6 +23,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
@@ -30,10 +35,14 @@ public class MainActivity extends Activity {
     Button rightButton; 
     Button driveButton; 
     Button backButton;
+    Button straightButton;
     Animation buttonEffect;
     Drawable d;
     public static final int ServerPort = 4359;
     String ServerIP;
+    Socket socket;
+    PrintWriter printWriter;
+    DataOutputStream DOS;
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,223 +56,250 @@ public class MainActivity extends Activity {
         // Set an EditText view to get user input 
         final EditText input = new EditText(this);
         alert.setView(input);
-
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-        public void onClick(DialogInterface dialog, int whichButton) {
-		          ServerIP = input.getText().toString();
-		          // Do something with value!
-		          final TextView reading = (TextView) findViewById(R.id.reading);
-		          SeekBar seekBar = (SeekBar)findViewById(R.id.seekbar);
-		          seekBar.setMax(100);
-		          
-		          if (ServerIP != null)
-		          	reading.setText("ServerIP: "+ServerIP);
-		          
-		          try {
-		  			Socket socket = new Socket(ServerIP, ServerPort);
-		  			if (socket.isConnected())
-		  				reading.setText("Connection succeeds!");
-		  			else
-		  				reading.setText("Connection fails!");
-		  	        socket.close();
-		  	       
-		  		} catch (UnknownHostException e) {
-		  		        
-		  		        e.printStackTrace();
-		  		} catch (IOException e) {
-		  		        
-		  		        e.printStackTrace();
-		  		}
-
-		          seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-		     
-		  		   @Override
-		  		   public void onStopTrackingTouch(SeekBar seekBar) {
-		  		   }
-		  		   
-		  		   @Override
-		  		   public void onStartTrackingTouch(SeekBar seekBar) {
-		  		    
-		  		   }
-		  		   
-		  		   @Override
-		  		   public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-		  			   reading.setText("Speed "+progress+ " cm/s");
-		  			   try {
-		  					Socket socket = new Socket(ServerIP, ServerPort);
-		  			        DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
-		  			        DOS.writeUTF("Speed "+progress+ " cm/s");
-		  			        socket.close();
-		  			       
-		  				} catch (UnknownHostException e) {
-		  				        
-		  				        e.printStackTrace();
-		  				} catch (IOException e) {
-		  				        
-		  				        e.printStackTrace();
-		  				}
-		  		   }
-		  		});
-		          
-		          buttonEffect = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_effect);
-		          
-		          mainButton = (Button) findViewById(R.id.button_main);
-		          mainButton.setOnClickListener(new OnClickListener(){
-		          	@Override
-		              public void onClick(View v){
-		          		mainButton.startAnimation(buttonEffect);
-		          		try {
-		  					Socket socket = new Socket(ServerIP, ServerPort);
-		  			        DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
-		  			        DOS.writeUTF("EXIT");
-		  			        socket.close();
-		  			       
-		  				} catch (UnknownHostException e) {
-		  				        
-		  				        e.printStackTrace();
-		  				} catch (IOException e) {
-		  				        
-		  				        e.printStackTrace();
-		  				}
-		                  finish();
-		          	}
-		          });
-		          	
-
-		          leftButton = (Button) findViewById(R.id.button_left);
-		          rightButton = (Button) findViewById(R.id.button_right);
-		          driveButton = (Button) findViewById(R.id.button_forward);
-		          backButton = (Button) findViewById(R.id.button_backward);
-		          d = (Drawable) mainButton.getBackground();
-		          
-		          leftButton.setOnTouchListener(new View.OnTouchListener() {
-		  			@Override
-		  			public boolean onTouch(View v, MotionEvent event) {
-		  				
-		  				if(event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
-		  					leftButton.setBackgroundColor(Color.CYAN);
-		  					try {
-		  						Socket socket = new Socket(ServerIP, ServerPort);
-		  				        DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
-		  				        DOS.writeUTF("LEFT");
-		  				        socket.close();
-		  				       
-		  					} catch (UnknownHostException e) {
-		  					        
-		  					        e.printStackTrace();
-		  					} catch (IOException e) {
-		  					        
-		  					        e.printStackTrace();
-		  					}
-
-		  				}
-		  				else if(event.getAction() == MotionEvent.ACTION_UP)
-		  					leftButton.setBackgroundDrawable(d);
-		  				return true;
-		  			}
-		  		});
-		          
-		          
-		          rightButton.setOnTouchListener(new View.OnTouchListener() {
-		  			
-		  			@Override
-		  			public boolean onTouch(View v, MotionEvent event) {
-		  				
-		  				if(event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
-		  					rightButton.setBackgroundColor(Color.GREEN);
-		  					try {
-		  						Socket socket = new Socket(ServerIP, ServerPort);
-		  				        DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
-		  				        DOS.writeUTF("RIGHT");
-		  				        socket.close();
-		  				       
-		  					} catch (UnknownHostException e) {
-		  					        
-		  					        e.printStackTrace();
-		  					} catch (IOException e) {
-		  					        
-		  					        e.printStackTrace();
-		  					}
-		  				}
-		  				else if(event.getAction() == MotionEvent.ACTION_UP)
-		  					rightButton.setBackgroundDrawable(d);
-		  				return true;
-		  			}
-		  		});
-		          
-		          driveButton.setOnTouchListener(new View.OnTouchListener() {
-		  			
-		  			@Override
-		  			public boolean onTouch(View v, MotionEvent event) {
-		  				
-		  				if(event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
-		  					driveButton.setBackgroundColor(Color.BLUE);
-		  					try {
-		  						Socket socket = new Socket(ServerIP, ServerPort);
-		  				        DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
-		  				        DOS.writeUTF("DRIVE");
-		  				        socket.close();
-		  				       
-		  					} catch (UnknownHostException e) {
-		  					        
-		  					        e.printStackTrace();
-		  					} catch (IOException e) {
-		  					        
-		  					        e.printStackTrace();
-		  					}
-		  				}
-		  				else if(event.getAction() == MotionEvent.ACTION_UP)
-		  					driveButton.setBackgroundDrawable(d);
-		  				return true;
-		  			}
-		  		});
-		          
-		          backButton.setOnTouchListener(new View.OnTouchListener() {
-		  			
-		  			@Override
-		  			public boolean onTouch(View v, MotionEvent event) {
-		  				
-		  				if(event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE) {
-		  					backButton.setBackgroundColor(Color.YELLOW);
-		  					try {
-		  						Socket socket = new Socket(ServerIP, ServerPort);
-		  				        DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
-		  				        DOS.writeUTF("BACK");
-		  				        socket.close();
-		  				       
-		  					} catch (UnknownHostException e) {
-		  					        e.printStackTrace();
-		  					} catch (IOException e) {
-		  					        e.printStackTrace();
-		  					}
-		  				}
-		  				else if(event.getAction() == MotionEvent.ACTION_UP)
-		  					backButton.setBackgroundDrawable(d);
-		  				return true;
-		  			}
-		  		});
-          }
-        });
-
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-          public void onClick(DialogInterface dialog, int whichButton) {
-            // Canceled.
-        	  dialog.cancel();
-        	  finish();
-          }
-        });
-
-        alert.show();
         
-        /*
-        //sleep 2 seconds
-        Handler handler = new Handler(); 
-        handler.postDelayed(new Runnable() { 
-             public void run() { 
-             } 
-        }, 2000);
-        */	
+          
+	        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int whichButton) {
+			          ServerIP = input.getText().toString();
+			          // Do something with value!
+			          final TextView reading = (TextView) findViewById(R.id.reading);
+			          SeekBar seekBar = (SeekBar)findViewById(R.id.seekbar);
+			          seekBar.setMax(255);
+			          
+			          if (ServerIP != null)
+			          	reading.setText("ServerIP: "+ServerIP);      
+			         
+			          try {
+						  socket = new Socket(ServerIP, ServerPort);
+						  printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+						  DOS = new DataOutputStream(socket.getOutputStream());
+			        	  
+						  if (socket.isConnected())
+				  				reading.setText("Connection succeeds!");
+				  			else
+				  				reading.setText("Connection fails!");
+	
+			          seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			     
+			  		   @Override
+			  		   public void onStopTrackingTouch(SeekBar seekBar) {
+			  		   }
+			  		   
+			  		   @Override
+			  		   public void onStartTrackingTouch(SeekBar seekBar) {
+			  		    
+			  		   }
+			  		   
+			  		   @Override
+			  		   public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+			  			   reading.setText("Speed "+progress+ " cm/s");
+			  			   
+			  			   if(progress <= 64){
+			  				   seekBar.setBackgroundColor(Color.RED);
+			  			   }else{
+			  				  seekBar.setBackgroundColor(Color.GREEN);
+			  				  //seekBar.setProgressDrawable(new ColorDrawable(Color.GREEN));
+			  				  //setProgressBarColor(seekBar, Color.GREEN);
+			  			   }
+			  			 
+			  			 try {
+							DOS.writeUTF("Stop");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+			  			 printWriter.print(Integer.toString(progress));//anscheinend printWriter nicht funktioniert??
+			  			 printWriter.flush();
+			  			   
+			  		   }
+			  		});
+			          
+			          buttonEffect = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_effect);
+			          
+			          mainButton = (Button) findViewById(R.id.button_main);
+			          mainButton.setOnClickListener(new OnClickListener(){
+			          	@Override
+			              public void onClick(View v){
+			          		mainButton.startAnimation(buttonEffect);
+			          		try {
+								DOS.writeUTF("Exit");
+								socket.close();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+			          		printWriter.print("Exit");
+				  			printWriter.flush();
+				  			
+			                finish();
+			          	}
+			          });
+			          	
+			          straightButton = (Button) findViewById(R.id.button_straight);
+			          leftButton = (Button) findViewById(R.id.button_left);
+			          rightButton = (Button) findViewById(R.id.button_right);
+			          driveButton = (Button) findViewById(R.id.button_forward);
+			          backButton = (Button) findViewById(R.id.button_backward);
+			          d = (Drawable) mainButton.getBackground();
+			          
+			          leftButton.setOnClickListener(new OnClickListener(){
+			  			@Override
+			  			public void onClick(View v){
+			  					leftButton.setBackgroundColor(Color.CYAN);
+			  					rightButton.setBackgroundDrawable(d);
+			  					straightButton.setBackgroundDrawable(d);
+			  					try {
+									DOS.writeUTF("Left");
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+			  					printWriter.print("Left");
+					  			printWriter.flush();
+			  								  				}
+			  		  });
+			          
+			          straightButton.setOnClickListener(new OnClickListener(){
+				  			@Override
+				  			public void onClick(View v){
+				  					straightButton.setBackgroundColor(Color.RED);
+				  					rightButton.setBackgroundDrawable(d);
+				  					leftButton.setBackgroundDrawable(d);
+				  					try {
+										DOS.writeUTF("Straight");
+									} catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+				  					printWriter.print("Straight");
+						  			printWriter.flush();		
+				  				}
+				  		  });
+			          
+			          rightButton.setOnClickListener(new OnClickListener(){
+			  			
+			        	  @Override
+				  			public void onClick(View v){
+				  					rightButton.setBackgroundColor(Color.GREEN);
+				  					leftButton.setBackgroundDrawable(d);
+				  					straightButton.setBackgroundDrawable(d);
+				  					try {
+										DOS.writeUTF("Right");
+									} catch (IOException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+				  					printWriter.print("Right");
+						  			printWriter.flush();
+				  				
+				  				}
+				  		});
+			          
+			          driveButton.setOnTouchListener(new View.OnTouchListener() {
+			  			
+			  			@Override
+			  			public boolean onTouch(View v, MotionEvent event) {		  				
+			  				if(event.getAction() == MotionEvent.ACTION_DOWN) {
+			  					driveButton.setBackgroundColor(Color.BLUE);
+			  					try {
+									DOS.writeUTF("Forwards");
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+			  					printWriter.print("Forwards");
+					  			printWriter.flush();
+			  						
+			  				}
+			  				else if(event.getAction() == MotionEvent.ACTION_UP)
+			  				{
+			  					driveButton.setBackgroundDrawable(d);
+			  					try {
+									DOS.writeUTF("Stop");
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+			  					printWriter.print("Stop");
+					  			printWriter.flush();
+			  					
+			  				}
+			  				return true;
+			  			}
+			  		});
+			          
+			          backButton.setOnTouchListener(new View.OnTouchListener() {
+			  			
+			  			@Override
+			  			public boolean onTouch(View v, MotionEvent event) {
+			  				if(event.getAction() == MotionEvent.ACTION_DOWN){
+			  					backButton.setBackgroundColor(Color.YELLOW);
+			  					try {
+									DOS.writeUTF("Backwards");
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+			  					printWriter.print("Backwards");
+					  			printWriter.flush();
+			  					
+			  				}
+			  				else if(event.getAction() == MotionEvent.ACTION_UP)
+			  				{
+			  					backButton.setBackgroundDrawable(d);
+			  					try {
+									DOS.writeUTF("Stop");
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+			  					printWriter.print("Stop");
+					  			printWriter.flush();
+			  					
+			  					/*
+			  					try {
+				  						Socket socket = new Socket(ServerIP, ServerPort);
+				  				        DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());			  				
+				  				        DOS.writeUTF("Stop");
+				  				        socket.close();  				        
+			  					} catch (UnknownHostException e) {
+			  					        e.printStackTrace();
+			  					} catch (IOException e) {
+			  					        e.printStackTrace();
+			  					}*/
+			  				}
+			  				return true;
+			  			}
+			  		});
+			          
+		          
+		          } catch (UnknownHostException e) {
+		  	        e.printStackTrace();
+		          } catch (IOException e) {    
+		  	        e.printStackTrace();
+		          }
+	          }
+	        });
+	
+	        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+	          public void onClick(DialogInterface dialog, int whichButton) {
+	            // Canceled.
+	        	  dialog.cancel();
+	        	  finish();
+	          }
+	        });
+        
+	        alert.show();
+        
 	}
+	
+	public void setProgressBarColor(ProgressBar progressBar, int newColor){ 
+	    LayerDrawable ld = (LayerDrawable) progressBar.getProgressDrawable();
+	    ClipDrawable d1 = (ClipDrawable) ld.findDrawableByLayerId(R.id.progressshape);
+	    d1.setColorFilter(newColor, PorterDuff.Mode.SRC_IN);
+
+	}
+	
 }
 
 
