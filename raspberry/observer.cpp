864ea6e4b820c26815a9245ed748043500efdcfb
kjkuhn <kuhn@in.tum.de>
2014-01-21 19:17:59 +0100

    debug

--- raspberry/observer.cpp
+++ raspberry/observer.cpp
@@ -1,5 +1,8 @@
 #include "observer.h"
 
+using namespace std;
+
+
 	Observer::Observer(rasp_sock::RaspberrySocket *sock,USBPipe *pipe){
 		this->sock = sock;
 		this->pipe = pipe;
@@ -12,28 +15,43 @@
 
 	void Observer::drive(uint8_t direction)
 	{
+#ifdef RASP_DEBUG
+		cout << "driving to: " << direction << endl;
+#endif
 		pipe->usbWrite((void*)&direction,sizeof(direction));
 	}
 
 	void Observer::steer(uint8_t direction)
 	{
+#ifdef RASP_DEBUG
+		cout << "steering to: " << direction << endl;
+#endif
 		pipe->usbWrite((void*)&direction,sizeof(direction));
 	}
 
 	void Observer::moveFrontLaser(uint8_t degrees)
 	{
+#ifdef RASP_DEBUG
+		cout << "moving frontLaser: " << degrees << endl;
+#endif
 		pipe->usbWrite((void*)&CMD_MOVE_LASER_FRONT,sizeof(uint8_t));
 		pipe->usbWrite((void*)&degrees,sizeof(degrees));
 	}
 
 	void Observer::moveBackLaser(uint8_t degrees)
 	{
+#ifdef RASP_DEBUG
+		cout << "moving backLaser: " << degrees << endl;
+#endif
 		pipe->usbWrite((void*)&CMD_MOVE_LASER_BACK,sizeof(uint8_t));
 		pipe->usbWrite((void*)&degrees,sizeof(degrees));
 	}
 		
 	void Observer::getFrontDistance(uint8_t* dist)
 	{
+#ifdef RASP_DEBUG
+		cout << "reading front dist" << endl;
+#endif
 		pipe->usbFlush();
 		pipe->usbWrite((void*)&CMD_GET_LASERDATA_FRONT,sizeof(uint8_t));
 		pipe->usbRead(fromArduino.comc.laserDataFront,LASERDATA_LEN,LASERDATA_LEN);
@@ -45,6 +63,9 @@
 
 	uint8_t Observer::getBackDistance()
 	{
+#ifdef RASP_DEBUG
+		cout << "reading back dist" << endl;
+#endif
 		pipe->usbFlush();
 		pipe->usbWrite((void*)&CMD_GET_LASERDATA_BACK,sizeof(uint8_t));
 		pipe->usbRead(&fromArduino.comc.laserDataBack,sizeof(uint8_t));
@@ -56,7 +77,9 @@
 		std::string str;
 		while(1){
 			if(toArduino.changed.load(std::memory_order_relaxed)){
+#ifdef RASP_DEBUG
 				std::cout<<"nh"<<std::endl;
+#endif
 				toArduino.mtx.lock();
 				toArduino.changed.store(false, std::memory_order_relaxed);
 				if(toArduino.comc.direction == -1)
