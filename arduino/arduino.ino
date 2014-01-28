4d0d3c9ea7a0d32820c0f833d94c81a63b31d1c5
Josef <j.stark@tum.de>
2014-01-28 13:53:17 +0100

    fix arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -78,11 +78,17 @@ void loop()
       backServo.write(getNextByte());
       break;
     case CMD_GET_LASERDATA_FRONT:
-      serialData[0]=frontIR0.getData();
+      /*serialData[0]=frontIR0.getData();
       serialData[1]=frontIR1.getData();
       serialData[2]=frontIR2.getData();
       serialData[3]=frontIR3.getData();
       serialData[4]=frontIR4.getData();
+      */
+      serialData[0]=5;
+      serialData[1]=6;
+      serialData[2]=7;
+      serialData[3]=8;
+      serialData[4]=9;
       Serial.write((char*)serialData);
       break;
     case CMD_GET_LASERDATA_BACK:
