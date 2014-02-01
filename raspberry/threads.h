506b78eabb9ae07da31c71366f22139f6d224cef
kjkuhn <kuhn@in.tum.de>
2014-02-01 13:36:21 +0100

    fix

--- raspberry/threads.h
+++ raspberry/threads.h
@@ -8,5 +8,5 @@
 #include "pathfinding.h"
 
 void socketThreadEntry(rasp_sock::RaspberrySocket*, comSync*, Observer*);
-void pathFindingThread(Observer*);
+void pathFindingThread(Observer*, int, int);
 #endif
