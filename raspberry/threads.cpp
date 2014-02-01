7d6011607dfcc7289d9f01495460793d50bc588b
kjkuhn <kuhn@in.tum.de>
2014-02-01 12:55:20 +0100

    fix

--- raspberry/threads.cpp
+++ raspberry/threads.cpp
@@ -21,46 +21,46 @@ wait_for_client:
 		str = sPtr->receive();
 		std::cout<<str<<std::endl;
 		if(str.compare(COMMAND_FWD) == 0){
-			//o->drive(DIR_FWD);
-			cPtr->mtx.lock();
+			o->drive(DIR_FWD);
+			/*cPtr->mtx.lock();
 			cPtr->comc.direction = 1;
 			cPtr->mtx.unlock();
-			cPtr->changed.store(true, std::memory_order_relaxed);
+			cPtr->changed.store(true, std::memory_order_relaxed);*/
 		}
 		else if(str.compare(COMMAND_RWD) == 0){
-			//o->drive(DIR_RWD);
-			cPtr->mtx.lock();
+			o->drive(DIR_RWD);
+			/*cPtr->mtx.lock();
 			cPtr->comc.direction = -1;
 			cPtr->mtx.unlock();
-			cPtr->changed.store(true, std::memory_order_relaxed);
+			cPtr->changed.store(true, std::memory_order_relaxed);*/
 		}
 		else if(str.compare(COMMAND_LEFT) == 0){
-			//o->steer(LEFT);
-			cPtr->mtx.lock();
+			o->steer(LEFT);
+			/*cPtr->mtx.lock();
 			cPtr->comc.steering = -1;
 			cPtr->mtx.unlock();
-			cPtr->changed.store(true, std::memory_order_relaxed);
+			cPtr->changed.store(true, std::memory_order_relaxed);*/
 		}
 		else if(str.compare(COMMAND_RIGHT) == 0){
-			//o->steer(RIGHT);
-			cPtr->mtx.lock();
+			o->steer(RIGHT);
+			/*cPtr->mtx.lock();
 			cPtr->comc.steering = 1;
 			cPtr->mtx.unlock();
-			cPtr->changed.store(true, std::memory_order_relaxed);
+			cPtr->changed.store(true, std::memory_order_relaxed);*/
 		}
 		else if(str.compare(COMMAND_STRAIGHT) == 0){
-			//o->steer(STRAIGHT);
-			cPtr->mtx.lock();
+			o->steer(STRAIGHT);
+			/*cPtr->mtx.lock();
 			cPtr->comc.steering = 0;
 			cPtr->mtx.unlock();
-			cPtr->changed.store(true, std::memory_order_relaxed);
+			cPtr->changed.store(true, std::memory_order_relaxed);*/
 		}
 		else if(str.compare(COMMAND_STOP) == 0){
-			//o->drive(DIR_STOP);
-			cPtr->mtx.lock();
+			o->drive(DIR_STOP);
+			/*cPtr->mtx.lock();
 			cPtr->comc.direction = 0;
 			cPtr->mtx.unlock();
-			cPtr->changed.store(true, std::memory_order_relaxed);
+			cPtr->changed.store(true, std::memory_order_relaxed);*/
 		}
 		size_t blank = str.find_first_of(' ');
 		command = str.substr(0, blank);
